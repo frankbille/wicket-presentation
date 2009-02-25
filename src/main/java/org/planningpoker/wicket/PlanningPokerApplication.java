8deaba2e3049bfee1cf8de10a1473a5af939a133
Frank Bille <frankbille@apache.org>
2009-02-25 18:25:52 +0100

    Ajax show/hide for participantscomponent

--- src/main/java/org/planningpoker/wicket/PlanningPokerApplication.java
+++ src/main/java/org/planningpoker/wicket/PlanningPokerApplication.java
@@ -2,7 +2,7 @@ package org.planningpoker.wicket;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
-import org.planningpoker.wicket.pages.CountPage;
+import org.planningpoker.wicket.pages.FrontPage;
 
 /**
  * Wicket application
@@ -15,7 +15,7 @@ public class PlanningPokerApplication extends WebApplication {
 
 	@Override
 	public Class<? extends Page> getHomePage() {
-		return CountPage.class;
+		return FrontPage.class;
 	}
 
 }
--- src/main/java/org/planningpoker/wicket/pages/FrontPage.java
+++ src/main/java/org/planningpoker/wicket/pages/FrontPage.java
@@ -3,7 +3,11 @@ package org.planningpoker.wicket.pages;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.planningpoker.domain.Participant;
@@ -13,6 +17,8 @@ import org.planningpoker.wicket.components.ParticipantsComponent;
 
 public class FrontPage extends WebPage {
 
+	private boolean showParticipants = false;
+	
 	public FrontPage() {
 		IModel<List<Participant>> participantsModel = new LoadableDetachableModel<List<Participant>>() {
 			private static final long serialVersionUID = 1L;
@@ -30,8 +36,43 @@ public class FrontPage extends WebPage {
 			}			
 		};
 		
-		final ParticipantsComponent participantsComponent = new ParticipantsComponent("participants", participantsModel);
+		final ParticipantsComponent participantsComponent = new ParticipantsComponent("participants", participantsModel) {
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public boolean isVisible() {
+				return showParticipants;
+			}
+		};
+		participantsComponent.setOutputMarkupId(true);
+		participantsComponent.setOutputMarkupPlaceholderTag(true);
 		add(participantsComponent);
+		
+		// Link
+		AjaxFallbackLink<Void> participantsLink = new AjaxFallbackLink<Void>("participantsLink") {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				showParticipants = showParticipants == false;
+				
+				if (target != null) {
+					target.addComponent(this);
+					target.addComponent(participantsComponent);
+				}
+			}
+		};
+		participantsComponent.setOutputMarkupId(true);
+		add(participantsLink);
+		
+		participantsLink.add(new Label("label", new AbstractReadOnlyModel<String>() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public String getObject() {
+				return showParticipants ? "Hide participants" : "Show participants";
+			}
+		}));
 	}
 	
 }
